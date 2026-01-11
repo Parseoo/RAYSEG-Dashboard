469134dd9a527fa3c7649e6ea07c7f1c7f544f56
Daniela Salazar <carolinasegoviano98@gmail.com>
2026-01-11 17:33:55 -0600

    Corrección en formulario de seguridad en el módulo de mi perfil.

--- app/(root)/settings/my-profile/security.tsx
+++ app/(root)/settings/my-profile/security.tsx
@@ -7,7 +7,12 @@ export const SecuritySettings = () => {
     const [isEditing, setIsEditing] = useState(false)
 
     return (
-        <div className="mt-4 w-full">
+        <>
+        <div className='w-full max-h-max rounded-lg p-5 border'>
+                        <h1 className='font-[500] text-lg'>Seguridad</h1>
+                        <p className='text-md text-gray-500'>Gestiona la contraseña. Otros cambios se hacen por administración.</p>
+        
+                         <div className="mt-4 w-full">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
                 Contraseña
             </label>
@@ -30,6 +35,9 @@ export const SecuritySettings = () => {
                 Se enviará un correo con el enlace para actualizar tu contraseña.
             </p>
         </div>
+                    </div>
+        </>
+       
     )
 }
 
